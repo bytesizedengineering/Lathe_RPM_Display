bdc713feb1f3a8e0eef67229887480873342feff
byte sized <zhipps@bytesizedengineering.com>
2019-11-21 21:49:41 -0500

    Update LatheRPMDisplay.ino
    Seven segment displays how many seconds since last reset

--- Code/LatheRPMDisplay/LatheRPMDisplay.ino
+++ Code/LatheRPMDisplay/LatheRPMDisplay.ino
@@ -12,39 +12,81 @@
 #define DIGIT_3 5
 #define DIGIT_4 4
 
-int segments[]={SEGMENT_A,SEGMENT_B,SEGMENT_C,SEGMENT_D,SEGMENT_E,SEGMENT_F,SEGMENT_G,SEGMENT_DP};
+#define POV_DELAY 250 // delay needed for persistance of vision
+
+int segments[]={SEGMENT_A, SEGMENT_B, SEGMENT_C, SEGMENT_D, SEGMENT_E, SEGMENT_F, SEGMENT_G, SEGMENT_DP};
 int digits[]={DIGIT_1,DIGIT_2,DIGIT_3,DIGIT_4};
 
 void setup() {
-  for(int i=0; i<7; i++){
+	Serial.begin(9600);
+	for(int i=0; i<8; i++){
 		pinMode(segments[i],OUTPUT);
-  }
-  /*
-  pinMode(SEGMENT_A, OUTPUT);
-  pinMode(SEGMENT_B, OUTPUT);
-  pinMode(SEGMENT_C, OUTPUT);
-  pinMode(SEGMENT_D, OUTPUT);
-  pinMode(SEGMENT_E, OUTPUT);
-  pinMode(SEGMENT_F, OUTPUT);
-  pinMode(SEGMENT_G, OUTPUT);
-  pinMode(SEGMENT_DP, OUTPUT);
-  */
-  for(int i=0; i<4; i++){
-		pinMode(digits[i],OUTPUT);
-  }
+		digitalWrite(segments[i],LOW);
+	}
   
+	for(int i=0; i<4; i++){
+		pinMode(digits[i],OUTPUT);
+		digitalWrite(digits[i],LOW);
+	}	
 }
 
 void loop() {
+	displayValue(millis()/1000);
+	//displayValue(8673);
 	
-	for(int i=0; i<4; i++){
-		digitalWrite(digits[i], HIGH);
-		for(int j=0; j<7; j++){
-			digitalWrite(segments[j],HIGH);
-			delay(500);
-			digitalWrite(segments[j],LOW);
-			delay(500);
-		}
-		digitalWrite(digits[i], LOW);
+}
+
+void displayValue (int value){
+	if(value < 10000 && value > 0){
+		int tempVal = value;
+		int tempDigit = 0;
+		tempDigit = tempVal/1000; displayDigit(tempDigit); digitalWrite(digits[0], HIGH); delayMicroseconds(POV_DELAY); clear(); digitalWrite(digits[0], LOW); tempVal = tempVal - tempDigit*1000;
+		tempDigit = tempVal/100; displayDigit(tempDigit); digitalWrite(digits[1], HIGH); delayMicroseconds(POV_DELAY); clear(); digitalWrite(digits[1], LOW); tempVal = tempVal - tempDigit*100;
+		tempDigit = tempVal/10; displayDigit(tempDigit); digitalWrite(digits[2], HIGH); delayMicroseconds(POV_DELAY); clear(); digitalWrite(digits[2], LOW); tempVal = tempVal - tempDigit*10;
+		displayDigit(tempVal); digitalWrite(digits[3], HIGH); delayMicroseconds(POV_DELAY); clear(); digitalWrite(digits[3], LOW);
 	}
+	else{
+		Serial.println("Error: value out of range");
+	}
+}
+
+void displayDigit(int number){
+	switch (number){
+		case 0:
+			digitalWrite(SEGMENT_A,HIGH); digitalWrite(SEGMENT_B,HIGH); digitalWrite(SEGMENT_C,HIGH); digitalWrite(SEGMENT_D,HIGH); digitalWrite(SEGMENT_E,HIGH); digitalWrite(SEGMENT_F,HIGH);
+			break;
+		case 1:
+			digitalWrite(SEGMENT_B,HIGH); digitalWrite(SEGMENT_C,HIGH);
+			break;
+		case 2:
+			digitalWrite(SEGMENT_A,HIGH); digitalWrite(SEGMENT_B,HIGH); digitalWrite(SEGMENT_D,HIGH); digitalWrite(SEGMENT_E,HIGH); digitalWrite(SEGMENT_G,HIGH);
+			break;
+		case 3:
+			digitalWrite(SEGMENT_A,HIGH); digitalWrite(SEGMENT_B,HIGH); digitalWrite(SEGMENT_C,HIGH); digitalWrite(SEGMENT_D,HIGH); digitalWrite(SEGMENT_G,HIGH);
+			break;
+		case 4:
+			digitalWrite(SEGMENT_B,HIGH); digitalWrite(SEGMENT_C,HIGH); digitalWrite(SEGMENT_F,HIGH); digitalWrite(SEGMENT_G,HIGH);
+			break;
+		case 5:
+			digitalWrite(SEGMENT_A,HIGH); digitalWrite(SEGMENT_C,HIGH); digitalWrite(SEGMENT_D,HIGH); digitalWrite(SEGMENT_F,HIGH); digitalWrite(SEGMENT_G,HIGH);
+			break;
+		case 6:
+			digitalWrite(SEGMENT_A,HIGH); digitalWrite(SEGMENT_C,HIGH); digitalWrite(SEGMENT_D,HIGH); digitalWrite(SEGMENT_E,HIGH); digitalWrite(SEGMENT_F,HIGH); digitalWrite(SEGMENT_G,HIGH);
+			break;
+		case 7:
+			digitalWrite(SEGMENT_A,HIGH); digitalWrite(SEGMENT_B,HIGH); digitalWrite(SEGMENT_C,HIGH);
+			break;
+		case 8:
+			digitalWrite(SEGMENT_A,HIGH); digitalWrite(SEGMENT_B,HIGH); digitalWrite(SEGMENT_C,HIGH); digitalWrite(SEGMENT_D,HIGH); digitalWrite(SEGMENT_E,HIGH); digitalWrite(SEGMENT_F,HIGH); digitalWrite(SEGMENT_G,HIGH);
+			break;
+		case 9:
+			digitalWrite(SEGMENT_A,HIGH); digitalWrite(SEGMENT_B,HIGH); digitalWrite(SEGMENT_C,HIGH); digitalWrite(SEGMENT_D,HIGH); digitalWrite(SEGMENT_F,HIGH); digitalWrite(SEGMENT_G,HIGH);
+			break;
+		default:
+			digitalWrite(SEGMENT_A,LOW); digitalWrite(SEGMENT_B,LOW); digitalWrite(SEGMENT_C,LOW); digitalWrite(SEGMENT_D,LOW); digitalWrite(SEGMENT_E,LOW); digitalWrite(SEGMENT_F,LOW); digitalWrite(SEGMENT_G,LOW);
+	}
+}
+
+void clear(){
+		digitalWrite(SEGMENT_A,LOW); digitalWrite(SEGMENT_B,LOW); digitalWrite(SEGMENT_C,LOW); digitalWrite(SEGMENT_D,LOW); digitalWrite(SEGMENT_E,LOW); digitalWrite(SEGMENT_F,LOW); digitalWrite(SEGMENT_G,LOW);
 }
